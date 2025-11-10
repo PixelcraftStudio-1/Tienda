3e5863780640eb43d627456fae667abf18492404
PixelcraftStudio <alejandro.hacker.medina@gmail.com>
2025-11-10 18:09:44 -0300

    Delete menu.js

--- menu.js
+++
@@ -1,7 +0,0 @@
-// Agregar interacción opcional: alert al seleccionar un producto
-document.querySelectorAll('.submenu li a').forEach(item => {
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
-    alert('Seleccionaste: ' + item.innerText);
-  });
-});
